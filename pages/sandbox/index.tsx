0b60d2d389807c53f061da64b2678b4cfddc00e2
nubby saurus <nubby@mail.com>
2024-01-11 19:42:22 -0800

    convenient stopping place for creating InputArrays; cleaned up .next

--- pages/sandbox/index.tsx
+++ pages/sandbox/index.tsx
@@ -1,34 +1,48 @@
 import { FieldValues, useForm } from 'react-hook-form';
 import {
-  Input
+  Button,
+  chakra,
 } from '@chakra-ui/react';
 
 import Banner from '../../components/Banner';
+import InputArrayBuilder from '../../components/InputArrayBuilder';
 import styles from '../../styles/Home.module.css';
 
 function Sandbox() {
   const {
     register,
     formState: { isDirty },
+    getValues,
     handleSubmit,
   } = useForm<FieldValues>({
     defaultValues: {
-      test: "default"
+      test: ["default"]
     }
   });
+
+  const onSubmit = (args) => {
+    console.log(args)
+  }
 
   return (
     <div>
       <Banner />
       <main>
         <h1>Fun stuff lives here.</h1>
-        <p className={styles.grid}>
-          <Input
-            {...register(
-              "test"
-            )}
-          />
-        </p>
+        <chakra.form id="test-form" onSubmit={handleSubmit(onSubmit)}>
+          <p className={styles.grid}>
+            <InputArrayBuilder
+              getValues={getValues}
+              name="test"
+              register={register}
+            />
+          </p>
+          <Button
+            type="submit"
+          >
+            Submit answer
+          </Button>
+        </chakra.form>
       </main>
     </div>
   );
